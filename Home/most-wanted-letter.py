94590b8c910f132586570835178bf809cf6c3f1a
Clark Chung <clark.ck@gmail.com>
2015-12-11 15:10:04 +0800

    Add solution for The Most Wanted Letter
    The Most Wanted Letter: http://www.checkio.org/mission/most-wanted-letter/

---
+++ Home/most-wanted-letter.py
@@ -0,0 +1,18 @@
+from collections import Counter
+
+def checkio(text):
+
+    c = Counter(filter(lambda x:x.isalpha(), text.lower()))
+    return min(c.iteritems(), key=lambda x:(-x[1], x[0]))[0]
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio(u"Hello World!") == "l", "Hello test"
+    assert checkio(u"How do you do?") == "o", "O is most wanted"
+    assert checkio(u"One") == "e", "All letter only once."
+    assert checkio(u"Oops!") == "o", "Don't forget about lower case."
+    assert checkio(u"AAaooo!!!!") == "a", "Only letters."
+    assert checkio(u"abe") == "a", "The First."
+    print("Start the long test")
+    assert checkio(u"a" * 9000 + u"b" * 1000) == "a", "Long."
+    print("The local tests are done.")
