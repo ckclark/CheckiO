a5ca6a8f0d9ca0bf57afeaa0c71f145a4747bb2f
Clark Chung <clark.ck@gmail.com>
2015-12-13 03:23:39 +0800

    Add solution for Index Power
    Index Power: http://www.checkio.org/mission/index-power/

---
+++ Elementary/index-power.py
@@ -0,0 +1,11 @@
+def index_power(array, n):
+    if 0 <= n < len(array):
+        return array[n] ** n
+    return -1
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert index_power([1, 2, 3, 4], 2) == 9, "Square"
+    assert index_power([1, 3, 10, 100], 3) == 1000000, "Cube"
+    assert index_power([0, 1], 0) == 1, "Zero power"
+    assert index_power([1, 2], 3) == -1, "IndexError"
