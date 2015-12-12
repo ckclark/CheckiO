656c6447220c57a4a00677bf03e4c14e836d8c6f
Clark Chung <clark.ck@gmail.com>
2015-12-13 03:46:25 +0800

    Add solution for Three words
    Three words: http://www.checkio.org/mission/three-words/

---
+++ Elementary/three-words.py
@@ -0,0 +1,11 @@
+import re
+def checkio(words):
+    return max(len(x.split()) for x in re.split(r'\d+', words)) >= 3
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio(u"Hello World hello") == True, "Hello"
+    assert checkio(u"He is 123 man") == False, "123 man"
+    assert checkio(u"1 2 3 4") == False, "Digits"
+    assert checkio(u"bla bla bla bla") == True, "Bla Bla"
+    assert checkio(u"Hi") == False, "Hi"
