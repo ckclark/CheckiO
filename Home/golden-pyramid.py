9fb8638d05bf3ca785cca36a01f7c14185f9eb0b
Clark Chung <clark.ck@gmail.com>
2015-12-12 23:25:53 +0800

    Add solution for Golden Pyramid
    Golden Pyramid: https://www.checkio.org/mission/golden-pyramid/

---
+++ Home/golden-pyramid.py
@@ -0,0 +1,37 @@
+def count_gold(pyramid):
+    """
+    Return max possible sum in a path from top to bottom
+    """
+    ans = list(pyramid[-1])
+    for i in xrange(len(pyramid) - 2, -1, -1):
+        for j, v in enumerate(pyramid[i]):
+            ans[j] = v + max(ans[j], ans[j + 1])
+    return ans[0]
+
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert count_gold((
+        (1,),
+        (2, 3),
+        (3, 3, 1),
+        (3, 1, 5, 4),
+        (3, 1, 3, 1, 3),
+        (2, 2, 2, 2, 2, 2),
+        (5, 6, 4, 5, 6, 4, 3)
+    )) == 23, "First example"
+    assert count_gold((
+        (1,),
+        (2, 1),
+        (1, 2, 1),
+        (1, 2, 1, 1),
+        (1, 2, 1, 1, 1),
+        (1, 2, 1, 1, 1, 1),
+        (1, 2, 1, 1, 1, 1, 9)
+    )) == 15, "Second example"
+    assert count_gold((
+        (9,),
+        (2, 2),
+        (3, 3, 3),
+        (4, 4, 4, 4)
+    )) == 18, "Third example"
