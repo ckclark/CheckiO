3aea50c844206a784d19cdaa470cadae64118903
Clark Chung <clark.ck@gmail.com>
2015-12-14 01:11:42 +0800

    Add solution for Days Between
    Days Between: http://www.checkio.org/mission/days-diff/

---
+++ Elementary/days-diff.py
@@ -0,0 +1,10 @@
+from datetime import date
+
+def days_diff(date1, date2):
+    return abs((date(*date2) - date(*date1)).days)
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert days_diff((1982, 4, 19), (1982, 4, 22)) == 3
+    assert days_diff((2014, 1, 1), (2014, 8, 27)) == 238
+    assert days_diff((2014, 8, 27), (2014, 1, 1)) == 238
