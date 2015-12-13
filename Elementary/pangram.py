d6d7d11dc015e9b2ae02dc3e05a1af51469109ee
Clark Chung <clark.ck@gmail.com>
2015-12-14 01:14:04 +0800

    Add solution for Pangram
    Pangram: http://www.checkio.org/mission/pangram/

---
+++ Elementary/pangram.py
@@ -0,0 +1,8 @@
+def check_pangram(text):
+    return len(set(filter(lambda x:x.isalpha(), text.lower()))) == 26
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert check_pangram("The quick brown fox jumps over the lazy dog."), "brown fox"
+    assert not check_pangram("ABCDEF"), "ABC"
+    assert check_pangram("Bored? Craving a pub quiz fix? Why, just come to the Royal Oak!"), "Bored?"
