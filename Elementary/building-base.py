ec4e8b6cca7a68410c43ec7faf962eeebb3e2ecd
Clark Chung <clark.ck@gmail.com>
2015-12-14 10:42:01 +0800

    Add solution for Building Base
    Building Base: https://www.checkio.org/mission/building-base/

---
+++ Elementary/building-base.py
@@ -0,0 +1,43 @@
+class Building:
+    def __init__(self, south, west, width_WE, width_NS, height=10):
+        self.south = south
+        self.west = west
+        self.width_WE = width_WE
+        self.width_NS = width_NS
+        self.height = height
+
+    def corners(self):
+        south = self.south
+        north = self.south + self.width_NS
+        west = self.west
+        east = self.west + self.width_WE
+        return {
+            "north-east": [north, east],
+            "south-east": [south, east],
+            "south-west": [south, west],
+            "north-west": [north, west],
+        }
+
+    def area(self):
+        return self.width_WE * self.width_NS
+
+    def volume(self):
+        return self.area() * self.height
+
+    def __repr__(self):
+        return "Building({0.south}, {0.west}, {0.width_WE}, {0.width_NS}, {0.height})".format(self)
+
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    def json_dict(d):
+        return dict((k, list(v)) for k, v in d.items())
+
+    b = Building(1, 2, 2, 3)
+    b2 = Building(1, 2, 2, 3, 5)
+    assert json_dict(b.corners()) == {'north-east': [4, 4], 'south-east': [1, 4],
+                                      'south-west': [1, 2], 'north-west': [4, 2]}, "Corners"
+    assert b.area() == 6, "Area"
+    assert b.volume() == 60, "Volume"
+    assert b2.volume() == 30, "Volume2"
+    assert str(b) == "Building(1, 2, 2, 3, 10)", "String"
