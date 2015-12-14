b186a7ae7bdaab0cc45d07cddbcc15f7c476218a
Clark Chung <clark.ck@gmail.com>
2015-12-14 16:56:49 +0800

    Add solution for Weak Point
    Weak Point: http://www.checkio.org/mission/weak-point/

---
+++ Electronic-Station/weak-point.py
@@ -0,0 +1,27 @@
+def weak_point(matrix):
+    r = [0] * len(matrix)
+    c = [0] * len(matrix[0])
+    for i, row in enumerate(matrix):
+        for j, v in enumerate(row):
+            r[i] += v
+            c[j] += v
+    minr = min(r)
+    minc = min(c)
+    return [next(i for (i, v) in enumerate(r) if v == minr), next(i for (i, v) in enumerate(c) if v == minc)]
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert isinstance(weak_point([[1]]), (list, tuple)), "The result should be a list or a tuple"
+    assert list(weak_point([[7, 2, 7, 2, 8],
+                            [2, 9, 4, 1, 7],
+                            [3, 8, 6, 2, 4],
+                            [2, 5, 2, 9, 1],
+                            [6, 6, 5, 4, 5]])) == [3, 3], "Example"
+    assert list(weak_point([[7, 2, 4, 2, 8],
+                            [2, 8, 1, 1, 7],
+                            [3, 8, 6, 2, 4],
+                            [2, 5, 2, 9, 1],
+                            [6, 6, 5, 4, 5]])) == [1, 2], "Two weak point"
+    assert list(weak_point([[1, 1, 1],
+                            [1, 1, 1],
+                            [1, 1, 1]])) == [0, 0], "Top left"
