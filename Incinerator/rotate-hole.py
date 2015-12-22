5e1a3720ab31b6ac161e7af23993d8a3de57b209
Clark Chung <clark.ck@gmail.com>
2015-12-22 12:09:05 +0800

    Add solution for Rotate Hole
    Rotate Hole: http://www.checkio.org/mission/rotate-hole/

---
+++ Incinerator/rotate-hole.py
@@ -0,0 +1,14 @@
+def rotate(state, pipe_numbers):
+    l = len(state)
+    ans = []
+    for i in xrange(l):
+        if all(state[x - i] for x in pipe_numbers):
+            ans.append(i)
+    return ans
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert rotate([1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1], [0, 1]) == [1, 8], "Example"
+    assert rotate([1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1], [0, 1, 2]) == [], "Mission impossible"
+    assert rotate([1, 0, 0, 0, 1, 1, 0, 1], [0, 4, 5]) == [0], "Don't touch it"
+    assert rotate([1, 0, 0, 0, 1, 1, 0, 1], [5, 4, 5]) == [0, 5], "Two cannonballs in the same pipe"
